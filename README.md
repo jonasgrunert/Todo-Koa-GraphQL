3d6ca9ddb26a1299e494f8bd33bf7708484f29c7
jonasgrunert <jonas@grunert-berlin.de>
2018-03-01 13:54:17 +0100

    Setup Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Installation
+
+'cd app'  
+'npm install'  
+'cd ..'  
+'docker-compose build'  
+'docker-compose up'  
+  
+Frameworks
+ - Apollo GraphQl
+ - node-couchdb
+ - koajs
---
+++ app/src/app.js
@@ -0,0 +1,51 @@
+import Koa from 'koa';
+import KoaRouter from 'koa-router';
+import KoaBodyParser from 'koa-bodyparser';
+import { graphqlKoa, graphiqlKoa } from 'apollo-server-koa';
+import { makeExecutableSchema } from 'graphql-tools';
+
+// GraphQL Schema
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query { books: [Book] }
+  type Book { title: String, author: String }
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => books },
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+// Router
+const router = new KoaRouter();
+router
+  // GraphQL
+  .get('/graphql', graphiqlKoa({ endpointURL: '/graphql' }))
+  .post('/graphql', graphqlKoa({ schema }));
+
+const app = new Koa();
+
+app
+  .use(KoaBodyParser())
+  .use(router.routes());
+
+app.listen(3000);
+
